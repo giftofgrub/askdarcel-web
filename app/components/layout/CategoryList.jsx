903e0053f4ccec347feffd175484eda967e0a42b
Richard Xia <richardxia@richardxia.com>
2019-07-28 14:39:40 -0700

    Fix lint on app/components/layouts (#808)
    * Fix lint on layouts.
    
    * Remove commented out references to RelativeOpeningTime.

--- app/components/layout/CategoryList.jsx
+++ app/components/layout/CategoryList.jsx
@@ -1,24 +1,14 @@
 import React from 'react';
 import CategoryItem from './CategoryItem';
 
-/* eslint-disable react/no-multi-comp */
-export class CategoryList extends React.Component {
-  render() {
-    const categoryNodes = this.props.categories.map(category => (
-      <CategoryItem name={category.name} key={category.id} categoryid={category.id} />
-    ));
-
-    return (
-      <section className="category-list" role="main">
-        <div className="featured-categories">
-          <h2 className="featured-categories__title">Discover resources by category</h2>
-          <ul className="category-items">
-            {' '}
-            {categoryNodes}
-            {' '}
-          </ul>
-        </div>
-      </section>
-    );
-  }
-}
+export const CategoryList = ({ categories }) => (
+  <section className="category-list" role="main">
+    <div className="featured-categories">
+      <h2 className="featured-categories__title">Discover resources by category</h2>
+      <ul className="category-items">
+        {categories.map(category => (
+          <CategoryItem name={category.name} key={category.id} categoryid={category.id} />))}
+      </ul>
+    </div>
+  </section>
+);
